f666282d2ad4b093385fc9328821d84b342d4565
NoorDiep <125315520+NoorDiep@users.noreply.github.com>
2023-03-24 11:04:55 +0100

    Update Autoencoder.py
    Updated to ols

--- Autoencoder.py
+++ Autoencoder.py
@@ -1,18 +1,19 @@
+
+from keras.layers import Dense
+from keras import Input
+from keras import Model
+from statsmodels.tsa.ar_model import AutoReg
 from Drivers import get_data, forecast_accuracy, optimal_lag, getDataTablesFigures
-from statsmodels.tsa.arima.model import ARIMA
+
 import numpy as np
 import pandas as pd
-from keras.layers import Dense,Conv2D,MaxPooling2D,UpSampling2D
-from keras import Input, Model
-from keras import Model
-import itertools
-from sklearn.model_selection import GridSearchCV
 
 ########################################################################################################################
 # LOAD DATA
 ########################################################################################################################
 # Import data into 60% training, 20% validation and 20% test sets
-data, df, df_diff, date_train, date_test, date_train_diff, date_test_diff, depo, swap_dates, spread, spread_diff = get_data(lag=5)
+lag=5
+data, df, df_diff, date_train, date_test, date_train_diff, date_test_diff, depo, swap_dates, spread, spread_diff = get_data(lag=lag)
 
 X_train = df[0]
 X_val = df[1]
@@ -38,7 +39,7 @@ X_tv_diff = np.vstack((X_train_diff, X_val_diff))
 X_diff = np.vstack((X_tv_diff, X_test_diff))
 
 # Return tables and figures for Data section
-#getDataTablesFigures(data[0], data[1], pd.DataFrame(Y_diff), pd.DataFrame(X_diff), depo, swap_dates)
+getDataTablesFigures(data[0], data[1], pd.DataFrame(Y_diff), pd.DataFrame(X_diff), depo, swap_dates)
 
 #getDataTablesFigures(data[2], data[3], pd.DataFrame(X_diff), depo, swap_dates) # input: df_swap, df_drivers, swap_diff# input: df_swap, df_drivers, swap_diff
 
@@ -47,10 +48,6 @@ def forecast_acc(forecast, actual):
     mse = np.mean((forecast - actual) ** 2)  # MSE
     rmse = np.mean((forecast - actual) ** 2) ** .5  # RMSE
     pd.DataFrame([[mae, mse, rmse]], columns=['mae', 'mse', 'rmse'])
-
-########################################################################################################################
-# RUN CODE
-########################################################################################################################
 
 ###### Results with differences ######
 def buildAE(y_train, y_tv, y_test, n):
@@ -102,248 +99,66 @@ def buildAE(y_train, y_tv, y_test, n):
     return encoded_train, encoded_tv, encoded_test, decoder, opt_params
 
 
+
 def getForecastAE(decoder, x, y, y_swap, n_train, n_tv, n_test, h):
+    w = n_tv
+    f_i = []
+    f_iX = []
+    p_i = []
+    p_iX = []
+    q_iX = []
 
-
-    w = n_train
-
-
-
-
-    p_ARi = []
-    p_ARXi = []
-    q_ARXi = []
-    resultsAR1 = []
-    resultsAR2 = []
-    resultsAR3 = []
-    resultsAR4 = []
-    resultsAR5 = []
-    resultsAR7 = []
-    resultsAR10 = []
-    resultsAR15 = []
-    resultsAR20 = []
-    resultsAR30 = []
-
-    resultsARX1 = []
-    resultsARX2 = []
-    resultsARX3 = []
-    resultsARX4 = []
-    resultsARX5 = []
-    resultsARX7 = []
-    resultsARX10 = []
-    resultsARX15 = []
-    resultsARX20 = []
-    resultsARX30 = []
-
-
-    resultsARX = []
+    y_test = pd.DataFrame(y[n_tv:])
 
 
 
     for i in range(0, y.shape[1]):
 
+        p_AR1 = optimal_lag(x_train=0, x_tv=0, y_train=y[:n_train,i], y_tv=y[:n_tv,i], maxlags_p=10, maxlags_q=10, indicator=0)
+        p_ARX1, q_ARX1 = optimal_lag(x_train=x[:n_train], x_tv=x[:n_tv], y_train=y[:n_train,i], y_tv=y[:n_tv,i], maxlags_p=10, maxlags_q=10, indicator=1)
+        p_i.append(p_AR1)
+        p_iX.append(p_ARX1)
+        q_iX.append(q_ARX1)
 
-        p_AR1 = optimal_lag(x_train=0, x_tv=0, y_train=y[:n_train,i], y_tv=y[:n_tv,i], maxlags_p=10, maxlags_q=15, indicator=0)
-        p_ARX1, q_ARX1 = optimal_lag(x_train=x[:n_train], x_tv=x[:n_tv], y_train=y[:n_train,i], y_tv=y[:n_tv,i], maxlags_p=10, maxlags_q=15, indicator=1)
-        p_ARi.append(p_AR1)
-        p_ARXi.append(p_ARX1)
-        q_ARXi.append(q_ARX1)
-
-        f_k1 = []
-        f_k2 = []
-        f_k3 = []
-        f_k4 = []
-        f_k5 = []
-        f_k7 = []
-        f_k10 = []
-        f_k15 = []
-        f_k20 = []
-        f_k30 = []
-
-        f_kX1 = []
-        f_kX2 = []
-        f_kX3 = []
-        f_kX4 = []
-        f_kX5 = []
-        f_kX7 = []
-        f_kX10 = []
-        f_kX15 = []
-        f_kX20 = []
-        f_kX30 = []
+        f_k = []
+        f_kX = []
 
         # Perform rolling window forecasts
         for k in range(n_test - h + 1):
             print(i)
             print(k)
+
+            # Fit both models
+            AR = AutoReg(endog=y[:k+h+w-1, i], lags=p_AR1).fit()
+            ARX = AutoReg(endog=y[:k+h+w-1, i], exog=x[:k+h+w-1], lags=p_ARX1).fit()
+
             # # Forecast out-of-sample
-            f_j = []
-            for j in range(y.shape[1]):
-                train = y[k:w + k, j]
-
-                AR = ARIMA(train, order=(p_AR1, 0, 0)).fit()
-
-                preds_AE = AR.predict(start=n_tv + k, end=n_tv + k + h - 1,
-                                      dynamic=False)  # Dynamic equal to False means direct forecasts
-                f_j.append(preds_AE)
-            preds_AR = pd.DataFrame(f_j)
-            preds_AR_s = decoder.predict(preds_AR.T)
-
-            test = y_swap[n_tv+k:n_tv+k + h]
-            test = pd.DataFrame(test)
-
-            preds_AR = preds_AR.T.reset_index(drop=True).T
-            acc_AR = forecast_accuracy(preds_AR_s, test, df_indicator=1)
-            f_k1.append(acc_AR.iloc[0])
-            f_k2.append(acc_AR.iloc[1])
-            f_k3.append(acc_AR.iloc[2])
-            f_k4.append(acc_AR.iloc[3])
-            f_k5.append(acc_AR.iloc[4])
-            f_k7.append(acc_AR.iloc[5])
-            f_k10.append(acc_AR.iloc[6])
-            f_k15.append(acc_AR.iloc[7])
-            f_k20.append(acc_AR.iloc[8])
-            f_k30.append(acc_AR.iloc[9])
+            preds_AR = AR.predict(start=w+k, end=w+k+h-1, dynamic=False)
+            y_hatAR = preds_AR[h-1]
+            f_k.append(y_hatAR)
 
             # Forecast out-of-sample
-            f_jX = []
-            for j in range(y.shape[1]):
-                ytrain = y[k + q_ARX1:w + k, j]
-                xtrain = pd.DataFrame(x[k:w + k]).shift(q_ARX1).dropna()
+            preds_ARX = ARX.predict(start=w+k, end=w+k+h-1, exog=x[:w+k+h-1], exog_oos=x[w:k+h+w+k],
+                                    dynamic=False)
+            y_hatARX = preds_ARX[h-1]
+            f_kX.append(y_hatARX)
+        f_i.append(f_k)
+        f_iX.append(f_kX)
 
-                # Construct the model with optimized parameters
-                ARX = ARIMA(ytrain, exog=xtrain, order=(p_AR1, 0, 0)).fit()
-                # Forecast out-of-sample
-                preds_AE = ARX.predict(start=n_tv + k, end=n_tv + k + h - 1, dynamic=False, exog=x[k - w - n_test:k + h - w + q_ARX1 + k])  # Dynamic equal to False means direct forecasts
-                f_jX.append(preds_AE)
-            preds_ARX = pd.DataFrame(f_jX)
-            preds_ARX_s = decoder.predict(preds_ARX.T)
+    preds_AR = decoder.predict(pd.DataFrame(f_i).T)
+    preds_ARX = decoder.predict(pd.DataFrame(f_iX).T)
 
-            test = y_swap[n_tv + k:n_tv + k + h]
-            test = pd.DataFrame(test)
+    acc_AR = forecast_accuracy(pd.DataFrame(preds_AR), pd.DataFrame(y_swap[-n_test + h - 1:]), df_indicator=1)
+    acc_ARX = forecast_accuracy(pd.DataFrame(preds_ARX), pd.DataFrame(y_swap[-n_test + h - 1:]), df_indicator=1)
 
-            # Get forecast accuracy
-            acc_ARX = forecast_accuracy(preds_ARX_s, test, df_indicator=1)
-            f_kX1.append(acc_ARX.iloc[0])
-            f_kX2.append(acc_ARX.iloc[1])
-            f_kX3.append(acc_ARX.iloc[2])
-            f_kX4.append(acc_ARX.iloc[3])
-            f_kX5.append(acc_ARX.iloc[4])
-            f_kX7.append(acc_ARX.iloc[5])
-            f_kX10.append(acc_ARX.iloc[6])
-            f_kX15.append(acc_ARX.iloc[7])
-            f_kX20.append(acc_ARX.iloc[8])
-            f_kX30.append(acc_ARX.iloc[9])
+    resultsAR = pd.DataFrame(acc_AR, columns=['MEA', 'MSE'])
+    resultsAR['lag p'] = pd.DataFrame(p_i)
 
-        f_i1 = pd.DataFrame(f_k1)
-        means1 = np.mean(f_i1)
-        means1['lag p'] = p_AR1
-        f_iX1 = pd.DataFrame(f_kX1)
-        meansX1 = np.mean(f_iX1)
-        meansX1['lag p'] = p_ARX1
-        meansX1['lag q'] = q_ARX1
+    resultsARX = pd.DataFrame(acc_ARX, columns=['MEA', 'MSE'])
+    resultsARX['lag p'] = pd.DataFrame(p_iX)
+    resultsARX['lag q'] = pd.DataFrame(q_iX)
 
-        f_i2 = pd.DataFrame(f_k2)
-        means2 = np.mean(f_i2)
-        means2['lag p'] = p_AR1
-        f_iX2 = pd.DataFrame(f_kX2)
-        meansX2 = np.mean(f_iX2)
-        meansX2['lag p'] = p_ARX1
-        meansX2['lag q'] = q_ARX1
-
-        f_i3 = pd.DataFrame(f_k3)
-        means3 = np.mean(f_i3)
-        means3['lag p'] = p_AR1
-        f_iX3 = pd.DataFrame(f_kX3)
-        meansX3 = np.mean(f_iX3)
-        meansX3['lag p'] = p_ARX1
-        meansX3['lag q'] = q_ARX1
-
-        f_i4 = pd.DataFrame(f_k4)
-        means4 = np.mean(f_i4)
-        means4['lag p'] = p_AR1
-        f_iX4 = pd.DataFrame(f_kX4)
-        meansX4 = np.mean(f_iX4)
-        meansX4['lag p'] = p_ARX1
-        meansX4['lag q'] = q_ARX1
-
-        f_i5 = pd.DataFrame(f_k5)
-        means5 = np.mean(f_i5)
-        means5['lag p'] = p_AR1
-        f_iX5 = pd.DataFrame(f_kX5)
-        meansX5 = np.mean(f_iX5)
-        meansX5['lag p'] = p_ARX1
-        meansX5['lag q'] = q_ARX1
-
-        f_i7 = pd.DataFrame(f_k7)
-        means7 = np.mean(f_i7)
-        means7['lag p'] = p_AR1
-        f_iX7 = pd.DataFrame(f_kX7)
-        meansX7 = np.mean(f_iX7)
-        meansX7['lag p'] = p_ARX1
-        meansX7['lag q'] = q_ARX1
-
-        f_i10 = pd.DataFrame(f_k10)
-        means10 = np.mean(f_i10)
-        means10['lag p'] = p_AR1
-        f_iX10 = pd.DataFrame(f_kX10)
-        meansX10 = np.mean(f_iX10)
-        meansX10['lag p'] = p_ARX1
-        meansX10['lag q'] = q_ARX1
-
-        f_i15 = pd.DataFrame(f_k15)
-        means15 = np.mean(f_i15)
-        means15['lag p'] = p_AR1
-        f_iX15 = pd.DataFrame(f_kX15)
-        meansX15 = np.mean(f_iX15)
-        meansX15['lag p'] = p_ARX1
-        meansX15['lag q'] = q_ARX1
-
-        f_i20 = pd.DataFrame(f_k20)
-        means20 = np.mean(f_i20)
-        means20['lag p'] = p_AR1
-        f_iX20 = pd.DataFrame(f_kX20)
-        meansX20 = np.mean(f_iX20)
-        meansX20['lag p'] = p_ARX1
-        meansX20['lag q'] = q_ARX1
-
-
-        f_i30 = pd.DataFrame(f_k30)
-        means30 = np.mean(f_i30)
-        means30['lag p'] = p_AR1
-        f_iX30 = pd.DataFrame(f_kX30)
-        meansX30 = np.mean(f_iX30)
-        meansX30['lag p'] = p_ARX1
-        meansX30['lag q'] = q_ARX1
-
-        resultsAR1.append(means1)
-        resultsAR2.append(means2)
-        resultsAR3.append(means3)
-        resultsAR4.append(means4)
-        resultsAR5.append(means5)
-        resultsAR7.append(means7)
-        resultsAR10.append(means10)
-        resultsAR15.append(means15)
-        resultsAR20.append(means20)
-        resultsAR30.append(means30)
-
-        resultsARX1.append(meansX1)
-        resultsARX2.append(meansX2)
-        resultsARX3.append(meansX3)
-        resultsARX4.append(meansX4)
-        resultsARX5.append(meansX5)
-        resultsARX7.append(meansX7)
-        resultsARX10.append(meansX10)
-        resultsARX15.append(meansX15)
-        resultsARX20.append(meansX20)
-        resultsARX30.append(meansX30)
-
-    AR = pd.DataFrame([np.mean(pd.DataFrame(resultsAR1)), np.mean(pd.DataFrame(resultsAR2)), np.mean(pd.DataFrame(resultsAR3)),
-          np.mean(pd.DataFrame(resultsAR4)),np.mean(pd.DataFrame(resultsAR5)), np.mean(pd.DataFrame(resultsAR7)),
-        np.mean(pd.DataFrame(resultsAR10)),np.mean(pd.DataFrame(resultsAR15)),np.mean(pd.DataFrame(resultsAR20)), np.mean(pd.DataFrame(resultsAR30))])
-    ARX = pd.DataFrame(
-        [np.mean(pd.DataFrame(resultsARX1)), np.mean(pd.DataFrame(resultsARX2)), np.mean(pd.DataFrame(resultsARX3)), np.mean(pd.DataFrame(resultsARX4)), np.mean(pd.DataFrame(resultsARX5)),
-        np.mean(pd.DataFrame(resultsARX7)), np.mean(pd.DataFrame(resultsARX10)), np.mean(pd.DataFrame(resultsARX15)), np.mean(pd.DataFrame(resultsARX20)), np.mean(pd.DataFrame(resultsARX30))])
-    return AR,ARX
+    return resultsAR, resultsARX
 
 
 ae_train, ae_tv, ae_test, decoder, opt_parameters = buildAE(Y_train_diff, Y_tv_diff, Y_test_diff, n=3)
@@ -351,8 +166,39 @@ y = np.vstack((pd.DataFrame(ae_tv), pd.DataFrame(ae_test)))
 x = np.vstack((X_tv, X_test))
 y_swap = np.vstack((Y_tv_diff, Y_test_diff))
 
-AE_AR10, AE_ARX10 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=10)
-AE_AR1, AE_ARX1 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=1)
-AE_AR5, AE_ARX5 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=5)
-AE_AR30, AE_ARX30 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=30)
+AE3_AR10, AE3_ARX10 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=10)
+print(AE3_AR10, AE3_ARX10)
+AE3_AR1, AE3_ARX1 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=1)
+print(AE3_AR1, AE3_ARX1)
+AE3_AR5, AE3_ARX5 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=5)
+print(AE3_AR1, AE3_ARX1)
+AE3_AR20, AE3_ARX20 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=20)
+t=1
+
+ae_train, ae_tv, ae_test, decoder, opt_parameters = buildAE(Y_train_diff, Y_tv_diff, Y_test_diff, n=2)
+y = np.vstack((pd.DataFrame(ae_tv), pd.DataFrame(ae_test)))
+x = np.vstack((X_tv, X_test))
+y_swap = np.vstack((Y_tv_diff, Y_test_diff))
+
+AE2_AR10, AE2_ARX10 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=10)
+print(AE2_AR10, AE2_ARX10)
+AE2_AR1, AE2_ARX1 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=1)
+print(AE2_AR1, AE2_ARX1)
+AE2_AR5, AE2_ARX5 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=5)
+print(AE2_AR1, AE2_ARX1)
+AE2_AR20, AE2_ARX20 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=20)
+t=1
+
+ae_train, ae_tv, ae_test, decoder, opt_parameters = buildAE(Y_train_diff, Y_tv_diff, Y_test_diff, n=4)
+y = np.vstack((pd.DataFrame(ae_tv), pd.DataFrame(ae_test)))
+x = np.vstack((X_tv, X_test))
+y_swap = np.vstack((Y_tv_diff, Y_test_diff))
+
+AE4_AR10, AE4_ARX10 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=10)
+print(AE4_AR10, AE4_ARX10)
+AE4_AR1, AE4_ARX1 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=1)
+print(AE4_AR1, AE4_ARX1)
+AE4_AR5, AE4_ARX5 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=5)
+print(AE4_AR1, AE4_ARX1)
+AE4_AR20, AE4_ARX20 = getForecastAE(decoder, x, y, y_swap, n_train=len(ae_train), n_tv=len(ae_tv), n_test=len(ae_test), h=20)
 t=1
